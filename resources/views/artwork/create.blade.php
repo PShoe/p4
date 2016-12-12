07a940ddbd49d5a21fa89649cc5f12463bf59aad
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-12-12 16:07:08 -0500

    commented out if statement in create view

--- resources/views/artwork/create.blade.php
+++ resources/views/artwork/create.blade.php
@@ -20,69 +20,69 @@
         </select>
     </div>
 
-    <!-- @if($artist_id == 'New Artist')
-    <div>
-        <label>New Artist:
-            <input type='text' name='artist_id'>
-        </label>
-    </div>
-    @endif -->
+<!-- @if($artist_id == 'New Artist')
+<div>
+<label>New Artist:
+<input type='text' name='artist_id'>
+</label>
+</div>
+@endif -->
 
 
-    <div class='tag'>
+<div class='tag'>
 
-        @foreach($tags_for_checkboxes as $tag_id => $tag_name)
-        <input type='checkbox' value='{{ $tag_id }}' name='tags[]'>
-        {{ $tag_name }} <br>
-        @endforeach
+    @foreach($tags_for_checkboxes as $tag_id => $tag_name)
+    <input type='checkbox' value='{{ $tag_id }}' name='tags[]'>
+    {{ $tag_name }} <br>
+    @endforeach
 
-    </div>
+</div>
 
 
-    <div>
-        <br>
-        <label>Title of the piece:
-            <input type='text' id='title' name='title' value='{{ old('title') }}'>
-        </label>
-    </div>
-    <div class='error'>{{ $errors->first('title') }}</div>
+<div>
+    <br>
+    <label>Title of the piece:
+        <input type='text' id='title' name='title' value='{{ old('title') }}'>
+    </label>
+</div>
+<div class='error'>{{ $errors->first('title') }}</div>
 
-    <div>
-        <label>Date of completion:  'YYYY-DD-MM',
-            <input type='text' id='date' name='date' value='{{ old('date') }}'>
-        </label>
-    </div>
-    <div class='error'>{{ $errors->first('date') }}</div>
+<div>
+    <label>Date of completion:  'YYYY-DD-MM',
+        <input type='text' id='date' name='date' value='{{ old('date') }}'>
+    </label>
+</div>
+<div class='error'>{{ $errors->first('date') }}</div>
 
-    <div>
-        <label>What medium did you use?:
-            <input type='text' id='medium' name='medium' value='{{ old('medium') }}'>
-        </label>
-    </div>
-    <div class='error'>{{ $errors->first('medium') }}</div>
+<div>
+    <label>What medium did you use?:
+        <input type='text' id='medium' name='medium' value='{{ old('medium') }}'>
+    </label>
+</div>
+<div class='error'>{{ $errors->first('medium') }}</div>
 
-    <div>
-        <label>Brief Description:
-            <input type='text' id='description' name='description' value='{{ old('description') }}'>
-        </label>
-    </div>
-    <div class='error'>{{ $errors->first('description') }}</div>
+<div>
+    <label>Brief Description:
+        <input type='text' id='description' name='description' value='{{ old('description') }}'>
+    </label>
+</div>
+<div class='error'>{{ $errors->first('description') }}</div>
 
-    <div>
-        <input type='file' id='image' name='image'>
-    </div>
-    <div class='error'>{{ $errors->first('image') }}</div>
+<div>
+    <input type='file' id='image' name='image'>
+</div>
+<div class='error'>{{ $errors->first('image') }}</div>
 
-    <div>
-        <br>
-        <button type="submit">Add your work!</button>
-    </div>
+<div>
+    <br>
+    <button type="submit">Add your work!</button>
+</div>
 
-    <div class='error'>
-        @if(count($errors) > 0)
-        Please correct the errors above and try again.
-        @endif
-    </div>
+<div class='error'>
+    @if(count($errors) > 0)
+    Please correct the errors above and try again.
+    @endif
+</div>
 
 </form>
 
