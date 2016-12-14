2b8ef4bf1cc15544bfecd5feff3ae686a8a5bcf0
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-12-14 10:45:30 -0500

    added text and dropdown input to create form

--- resources/views/artwork/create.blade.php
+++ resources/views/artwork/create.blade.php
@@ -9,15 +9,30 @@
 
     {{ csrf_field() }}
 
-    <div>
+    <!-- <div>
         <label>Registered Artist:</label>
         <select name='artist_id'>
+            <option></option>
             @foreach($artists_for_dropdown as $artist_id => $artist)
             <option value='{{ $artist_id }}'>{{ $artist }}</option>
             @endforeach
         </select>
+    </div> -->
+
+    <div>
+        <select style="position:absolute;top:10px;left:20px;width:200px; height:25px;line-height:20px;margin:0;padding:0;" onchange="document.getElementById('displayValue').value=this.options[this.selectedIndex].text; document.getElementById('idValue').value=this.options[this.selectedIndex].value;">
+            <option value='{{ $artist_id }}'></option>
+            @foreach($artists_for_dropdown as $artist_id => $artist)
+            <option value='{{ $artist_id }}'>{{ $artist }}</option>
+            @endforeach
+        </select>
+        <input name="displayValue" placeholder="Add or Select an Artist" id="displayValue" style="position:absolute;top:10px;left:20px;width:183px;width:180px\9;#width:180px;height:23px; height:21px\9;#height:18px;border:1px solid #556;" onfocus="this.select()" type="text">
+        <input name='{{ $artist_id }}' id='{{ $artist_id }}' type="hidden">
     </div>
 
+
+<br>
+<br>
 <div class='tag'>
     <label>Tags</label>
     <br>
@@ -74,11 +89,11 @@
     @endif
 </div>
 
-</form>
-
-
 @foreach($errors->all() as $error)
 	{{ $error }}
 @endforeach
+
+</form>
+
 
 @endsection
